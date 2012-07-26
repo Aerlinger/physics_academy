f376d982e49ed80cd0aa6eff8d4728c7d201d07e
Anthony <aerlinger@gmail.com>
2012-07-26 16:23:35 -0400

    Added Faraday

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,48 @@
 //= require jquery_ujs
 //= require jquery-ui
 //= require bootstrap
-//= require_tree .
+// require_self
+
+//= require challenge
+
+//= require Faraday/utility
+//= require Faraday/geom
+//= require Faraday/main
+
+
+//= require Faraday/FindPathInfo
+//= require Faraday/CircuitNode
+//= require Faraday/RowInfo
+//= require Faraday/CirSim
+
+//= require Faraday/elms/CircuitElement
+//= require Faraday/elms/core/Diode
+//= require Faraday/elms/core/Inductor
+//= require Faraday/elms/CapacitorElm
+//= require Faraday/elms/InductorElm
+//= require Faraday/elms/ResistorElm
+//= require Faraday/elms/VoltageElm
+//= require Faraday/elms/WireElm
+//= require Faraday/elms/SwitchElm
+//= require Faraday/elms/Switch2Elm
+//= require Faraday/elms/OutputElm
+//= require Faraday/elms/OpAmpElm
+//= require Faraday/elms/SparkGapElm
+//= require Faraday/elms/CurrentElm
+//= require Faraday/elms/GroundElm
+//= require Faraday/elms/LogicInputElm
+//= require Faraday/elms/LogicOutputElm
+//= require Faraday/elms/ProbeElm
+//= require Faraday/elms/SweepElm
+//= require Faraday/elms/TextElm
+//= require Faraday/elms/RailElm
+//= require Faraday/elms/TransistorElm
+//= require Faraday/elms/JFetElm
+
+
+//= require Faraday/lib/Font
+//= require Faraday/lib/raphael-2.1.0
+
+//= require_tree ./Faraday/ui/
+
+//= require Faraday/Scope
