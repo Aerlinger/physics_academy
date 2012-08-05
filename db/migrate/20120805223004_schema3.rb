548a66c87c5bcd35971a286f07bcae780c0bc7a6
Anthony <aerlinger@gmail.com>
2012-08-05 18:41:28 -0400

    Fix error in migration file

--- db/migrate/20120805223004_schema3.rb
+++ db/migrate/20120805223004_schema3.rb
@@ -48,7 +48,7 @@ class Schema3 < ActiveRecord::Migration
 
   create_table "lessons", :force => true do |t|
     t.string   "title"
-    t.text     "description",
+    t.text     "description"
                t.datetime "created_at",                                    :null => false
     t.datetime "updated_at",                                    :null => false
     t.string   "introduction"
