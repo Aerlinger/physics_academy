7e83ac03d8585fd9da921cfeeb157e2f0670a072
Anthony <aerlinger@gmail.com>
2012-08-14 16:41:47 -0400

    Clean test specs, clean up header formatting on main page

--- spec/models/circuit_element_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe CircuitElement do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
--- spec/views/labs/mechanics.html.erb_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe "labs/mechanics.html.erb" do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
